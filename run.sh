0fb6c45e79581567c8a45bf95bbddbedb42f40d0
linjingchun <linjingchun@pudotech.com>
2021-12-22 15:13:47 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 rm speexenc -f
 rm speexdec -f
-gcc speexenc.c -I speex_ogg/speex/include/ -L./speex_ogg/speex/libspeex/.libs -lspeex -o speexenc
-gcc speexdec.c -I speex_ogg/speex/include/ -L./speex_ogg/speex/libspeex/.libs -lspeex -o speexdec
+gcc  speex_api.c speexenc.c -I speex_ogg/speex/include/ -L./speex_ogg/speex/libspeex/.libs -lspeex -o speexenc
+gcc  speex_api.c speexdec.c -I speex_ogg/speex/include/ -L./speex_ogg/speex/libspeex/.libs -lspeex -o speexdec
